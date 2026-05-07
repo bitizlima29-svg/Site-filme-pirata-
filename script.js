8d34b298d9053d63cac5803bd20248366728d935
Paulo <paulohsssoares@gmail.com>
2026-05-07 08:36:17 -0300

    final

---
+++ script.js
@@ -0,0 +1,3 @@
+function venderAlma() {
+    return window.confirm("Você concorda em vender sua alma, toda sua linhagem familiar, seus bens, seus orgãos e sua diginidade para assistir esse filme?")
+}
